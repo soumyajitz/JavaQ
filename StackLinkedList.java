5ec9acd28b54ef1172e7bce227f7dab205ef323b
soumyajitz <2014soumyajit@gmail.com>
2016-02-12 02:31:17 -0600

    Stack Implementation of Java via Linked List

---
+++ StackLinkedList.java
@@ -0,0 +1,88 @@
+package OCJP;
+// Created the Link class
+class Linked{
+	public int data;
+	public Linked Link;
+	// Initialize the constructor
+	Linked(int d){
+		data = d;
+	}
+	// Print the data values
+	public void display(){
+		System.out.print(data+" ");
+	}
+} // end of class Linked
+
+class List{
+	private Linked first;
+	// Initialize the link
+	public List(){
+		first = null;
+	}
+	
+	public boolean isEmpty(){
+		return (first == null);
+	}
+	
+	public void insertFirst(int d){
+		Linked l = new Linked(d);
+		l.Link = first;
+		first = l;
+	}
+	
+	public int deleteFirst(){
+		Linked temp = first;
+		first = first.Link;
+		return temp.data;
+	}
+	
+	public void displayList(){
+		Linked current = first;
+		while(current != null){
+			current.display();
+			current = current.Link;
+		}
+		System.out.println();
+	}
+	
+} // End of class List
+
+class LinkedStack{
+	private List ll;
+	LinkedStack(){
+		ll = new List();
+	}
+	
+	public void push(int i){
+		ll.insertFirst(i);
+	}
+	public int  pop(){
+		return ll.deleteFirst();
+	}
+	
+	public boolean isEmoty(){
+		return ll.isEmpty();
+	}
+	
+	public void displayStack(){
+		System.out.println("Stack Values");
+		ll.displayList();
+	}
+	
+}
+
+public class StackLinkedList {
+	public static void main(String[] args) {
+		LinkedStack ls = new LinkedStack();
+		ls.push(1);
+		ls.push(2);
+		ls.push(3);
+		ls.push(4);
+		ls.push(5);
+		ls.displayStack();
+		ls.pop();
+		ls.displayStack();
+		
+	}
+
+}
